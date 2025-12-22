7885507b84d43575b94fcb6c6ec606614d3a4e89
pixprocoder <makkobir79@gmail.com>
2025-12-23 01:35:06 +0200

    feat: problem 02 solution add

--- queries.sql
+++ queries.sql
@@ -5,9 +5,20 @@ SELECT * FROM vehicles;
 
 SELECT * FROM bookings;
 
--- problem 1 (join)
+-- problem 01 (join)
 SELECT b.booking_id, u.name as customer_name, v.vehicle_name, b.start_date, b.end_date, b.status
 FROM
     bookings AS b
     INNER JOIN users AS u ON b.user_id = u.user_id
     INNER JOIN vehicles as v ON b.vehicle_id = v.vehicle_id;
+
+-- problem 02 (exists)
+SELECT *
+from vehicles AS v
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM bookings b
+        WHERE
+            b.vehicle_id = v.vehicle_id
+    )
